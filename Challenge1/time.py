bd81be5dc6767782e45f38de35ddcc61b533d58a
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-08-20 00:10:27 +0300

    completed challenge1

--- Challenge1/time.py
+++ Challenge1/time.py
@@ -5,3 +5,7 @@ def convert_to_24_hour(hour, minute, period):
         hour = hour % 12
 
     return f"{hour:02d}{minute:02d}"
+
+# Directly use the function with example values and print the result
+print(convert_to_24_hour(8, 30, "am"))  # Output: "0830"
+print(convert_to_24_hour(12, 15, "am"))  # Output: "0015"
