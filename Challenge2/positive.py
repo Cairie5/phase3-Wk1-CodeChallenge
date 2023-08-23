2bc669e9671346fbe3d5667f2db0a8117e1a9805
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-08-23 10:43:05 +0300

    completed code challenge

--- Challenge2/positive.py
+++ Challenge2/positive.py
@@ -1,27 +1,10 @@
 # Define a function named positive_count with three arguments: a, b, and c.
 def positive_count(a, b, c):
-   
-    # Create a count variable to keep track of the number of positive integers.
-    count = 0
-    
-    # Check if the first integer 'a' is positive.
-    if a > 0:
-        count += 1
-    
-    # Check if the second integer 'b' is positive.
-    if b > 0:
-        count += 1
-    
-    # Check if the third integer 'c' is positive.
-    if c > 0:
-        count += 1
-    
-    # Check if exactly two out of the three integers are positive.
-    if count == 2:
-        return True
-    else:
-        return False
+    # Check if at least two out of the three numbers are positive
+    return (a > 0 and b > 0) or (a > 0 and c > 0) or (b > 0 and c > 0)
 
+# Call the positive_count function with arguments 10, 5, and 2.
+result = positive_count(10, 5, 2)
 
-print(positive_count(2, 4, -3))   # True
-print(positive_count(-4, 6, 0))   # False
+# Print the result returned by the positive_count function.
+print(result)
